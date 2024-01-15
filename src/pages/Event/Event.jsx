9a8ec61bc4f59eff736bc441ad5ef2d9421a6a61
majhianish <majhianish123@gmail.com>
2024-01-15 18:56:51 +0530

    Updated images and links.

--- src/pages/Event/Event.jsx
+++ src/pages/Event/Event.jsx
@@ -2,71 +2,22 @@ import React from 'react';
 import './Event.css';
 import { Link } from 'react-router-dom';
 import { Card } from 'react-bootstrap';
-import Event1Img from '../../utils/images/blog1-img.jpg';
-import Event2Img from '../../utils/images/blog2-img.jpg';
-import Event3Img from '../../utils/images/blog3-img.jpg';
-import Event4Img from '../../utils/images/blog4-img.jpg';
-import Event5Img from '../../utils/images/blog5-img.jpg';
-import Event6Img from '../../utils/images/blog6-img.jpg';
-import Event7Img from '../../utils/images/blog7-img.jpg';
-import Event8Img from '../../utils/images/blog8-img.jpg';
-import Event9Img from '../../utils/images/blog9-img.jpg';
+import Event1Img from '../../utils/images/past-event1-img.jpeg';
 
 const events = [
     {
         id: 1,
         img: [Event1Img],
-        title: 'Event 1',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
+        title: 'Learning Based Intelligent Imaging and Beyond',
+        description: 'An enthralling discourse on Learning Based Intelligent Imaging and Beyond by Dr. Srutarshi Banerjee, Postdoctoral Researcher at Argonne National Laboratory.'
     },
-    {
-        id: 2,
-        img: [Event2Img],
-        title: 'Event 2',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
-    },
-    {
-        id: 3,
-        img: [Event3Img],
-        title: 'Event 3',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
-    },
-    {
-        id: 4,
-        img: [Event4Img],
-        title: 'Alumni Lecture Series: Seminar by Prof. Ajoy Kumar Ray',
-        description: "Topic: Electronics in the age of Data Science. \nProfessor Ajoy Kumar Ray, a recipient of the prestigious Padma Shri award, served as the last Vice Chancellor of Bengal Engineering and Science University, Shibpur and later as the first Director of Indian Institute of Engineering, Science and Technology, Shibpur till 2018. After leading the institute with his experience and developing the academics with his constant guidance and support, he joined back at his alma mater, IIT Kharagpur. Professor Ray received his Bachelor's degree from Bengal Engineering College, Shibpur and his Ph.D and Master's from IIT Kharagpur where he joined as the faculty  of Electronics and Electrical Communication Engineering Department in 1980. He is the co-inventor of numerous US Patents jointly with Intel Corporation and with Texas Instruments and has also headed the School of Medical Science and Technology which has initiated projects on molecular imaging, medical instrumentation etc.  He has co-authored around 160 research papers and published five books by International Publication Houses, one of which is in Chinese.  Despite completing numerous research and consultancy projects sponsored by several agencies, he's still passionately pursuing research in Medical Imaging and Image processing, Pattern Recognition and Machine Intelligence."
-    },
-    {
-        id: 5,
-        img: [Event5Img],
-        title: 'Invited Lecture Series: Seminar by Dr. Srutarshi Banerjee',
-        description: "The Academic Society of Electronics and Telecommunications is delighted to announce an insightful session scheduled for January 11th, titled "Learning-based Intelligent Imaging and Beyond," featuring Dr. Srutarshi Banerjee. Dr. Banerjee, a distinguished Postdoctoral Researcher at the X-ray Science Division, Argonne National Laboratory, brings a wealth of expertise in computational imaging, machine learning, and neuromorphic computing."
-    },
-    {
-        id: 6,
-        img: [Event6Img],
-        title: 'Event 6',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
-    },
-    {
-        id: 7,
-        img: [Event7Img],
-        title: 'Event 7',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
-    },
-    {
-        id: 8,
-        img: [Event8Img],
-        title: 'Event 8',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
-    },
-    {
-        id: 9,
-        img: [Event9Img],
-        title: 'Event 9',
-        description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.'
-    },
+    // {
+    //     id: 2,
+    //     img: [Event2Img],
+    //     title: 'Semiconductors: A Brief History',
+    //     description: 'A reflection on the past and the development of semiconductors by Professor S. Ashok, Pennsylvania State University on the occasion of Golden Jubilee Celebration of our Department.'
+    // },
+    
 ];
 
 function Event() {
@@ -74,8 +25,8 @@ function Event() {
     <div className='blog-page'>
         <header className='height-75'>
             <div className='container h-100 d-flex flex-column align-items-center justify-content-center text-light'>
-                <h1 className='text-center fw-semibold'>Events</h1>
-                <p className='text-center w-75 mb-5'>ASETE will organize weekly lectures from the month of January in two formats: Invited Lectures and Alumni Seminars. We are honoured to have Professor Ajoy Kumar Ray, the erstwhile director of IIEST, Shibpur to deliver the first talk of the series.</p>
+                <h1 className='text-center fw-semibold'>Past Events</h1>
+                {/* <p className='text-center w-75 mb-5'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Mollitia voluptas suscipit ratione quod culpa, eius ad consequatur, dolor quasi nulla optio quo error tempora temporibus distinctio quidem asperiores dolore ex amet nam. Consequatur, odit corporis.</p> */}
             </div>
         </header>
 
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,31 +1,23 @@
 import React from 'react';
 import './Home.css';
 import { Link } from 'react-router-dom';
-import UpcomingEventImg from '../../utils/images/start-courses-img.jpg';
+import UpcomingEventImg from '../../utils/images/upcoming-event-img.jpeg';
 import { Card } from 'react-bootstrap';
-import Event1Img from '../../utils/images/blog1-img.jpg';
-import Event2Img from '../../utils/images/blog2-img.jpg';
-import Event3Img from '../../utils/images/blog3-img.jpg';
+import Event1Img from '../../utils/images/past-event1-img.jpeg';
 
 const events = [
     {
         id: 1,
         img: [Event1Img],
-        title: 'Event 1',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, fugit? Doloremque deserunt ipsum eaque, dolor tempore, minima nisi debitis, et quas voluptatibus nam ex. Necessitatibus eligendi ratione expedita! Porro, ut.'
+        title: 'Learning Based Intelligent Imaging and Beyond',
+        description: 'An enthralling discourse on Learning Based Intelligent Imaging and Beyond by Dr. Srutarshi Banerjee, Postdoctoral Researcher at Argonne National Laboratory.'
     },
-    {
-        id: 2,
-        img: [Event2Img],
-        title: 'Event 2',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, fugit? Doloremque deserunt ipsum eaque, dolor tempore, minima nisi debitis, et quas voluptatibus nam ex. Necessitatibus eligendi ratione expedita! Porro, ut.'
-    },
-    {
-        id: 3,
-        img: [Event3Img],
-        title: 'Event 3',
-        description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, fugit? Doloremque deserunt ipsum eaque, dolor tempore, minima nisi debitis, et quas voluptatibus nam ex. Necessitatibus eligendi ratione expedita! Porro, ut.'
-    }
+    // {
+    //     id: 2,
+    //     img: [Event2Img],
+    //     title: 'Semiconductors: A Brief History',
+    //     description: 'A reflection on the past and the development of semiconductors by Professor S. Ashok, Pennsylvania State University on the occasion of Golden Jubilee Celebration of our Department.'
+    // },
 ];
 
 function Home() {
@@ -34,8 +26,10 @@ function Home() {
         <header className='h-100 min-vh-100 d-flex align-items-center text-light'>
             <div className='container d-flex flex-column align-items-center'>
                 <h2>Welcome To</h2>
+                <br></br>
+                <br></br>
                 <h1 className='text-center fw-semibold'>Department of Electronics and Telecommunication</h1>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eaque, fugit? Doloremque deserunt ipsum eaque, dolor tempore, minima nisi debitis, et quas voluptatibus nam ex. Necessitatibus eligendi ratione expedita! Porro, ut.</p>
+                <p className='text-center w-75 mb-5'>Aspire to become one of the Leading Departments of this Country with International Repute in the broad domain of Electrical Sciences and Technology through the development of Professional Attitude, Analytical and Innovative Mindsets, and Operational Expertise in the field of Electronics and Tele-Communication Engineering.</p>
                 <div className='d-flex flex-column flex-sm-row align-items-center'>
                     <Link to="/contact">
                         <button type='button' className='btn btn-outline-light btn-lg mx-0 mx-sm-2 my-2 my-sm-0'>Contact Us</button>
@@ -49,9 +43,11 @@ function Home() {
                 <div className='row d-flex align-items-center justify-content-around'>
                     <div className='col-lg-5'>
                         <h2 className='text-capitalize'>Upcoming Event</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus, placeat.</p>
-                        <Link to="/event">
-                            <button type='button' className='btn btn-danger btn-lg mx-0 mx-sm-2 my-2 my-sm-0'>Learn More</button>
+                        <p>Topic : Electronics in the Age of Data Science</p>
+                        <p>Time : 18th January, 4:15pm</p>
+                        <p>Speaker : Prof. Ajoy Kumar Ray, Former Director of IIEST, Shibpur</p>
+                        <Link to="https://forms.gle/EJhT4YeN8iyryXTk9" target="_blank">
+                            <button type='button' className='btn btn-primary btn-lg mx-0 mx-sm-2 my-2 my-sm-0'>Learn More</button>
                         </Link>
                     </div>
                     <div className='col-lg-5 mt-5 mt-lg-0'>
@@ -80,7 +76,7 @@ function Home() {
                     ))}
                 </div>
                 <Link to="/event">
-                    <button type='button' className='btn btn-danger btn-lg mt-5'>More Past Events</button>
+                    <button type='button' className='btn btn-primary btn-lg mt-5'>More Past Events</button>
                 </Link>
             </div>
         </div>
